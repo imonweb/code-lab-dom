2d9e0401b0022fe2e3e1decec67971f8aa2748bb
Imon <contact@imonweb.co.uk>
2023-04-30 21:42:58 +0100

    css

---
+++ app.js
@@ -0,0 +1,20 @@
+// DOM Manipulation
+
+
+// GetElementById()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// GetElementByClassName()
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+// getElementsByTagName()
+const listItems = document.getElementsByClassName('li');
+console.log(listItems);
+
+// querySelector()
+
+
+// querySelectorAll()
